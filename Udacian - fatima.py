7fa87593f33d015202c81713777077dbb9c3b31b
fatyArrammah <fatimaarrammah@yahoo.com>
2018-10-20 09:21:39 -0300

    I update the class

---
+++ Udacian - fatima.py
@@ -0,0 +1,23 @@
+class enrollment:
+    def __init__(self,enrollment):
+        self.enrollment = enrollment
+    
+    
+    
+class Udacian:
+    def __init__(self, name,city ,enrollment , nanodgree,status):
+        self.name = name
+        self.city = city
+        self.enrollment = enrollment
+        self.nanodgree = nanodgree
+        self.status = status
+         
+    def print_udacian(self):
+        print(self.name+' '+self.city+' '+self.enrollment+' '+self.status+' '+self.nanodgree)
+     
+        
+        
+             
+p = Udacian('fatima','jeddah','enrolment','full stack ','single')
+
+p.print_udacian()

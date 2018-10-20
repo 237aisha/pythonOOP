a46511788383186a8b40d66c922a1ac2bffa1132
Aisha Abdullah <237aisha@gmail.com>
2018-10-20 15:34:58 +0300

    I update your file

--- Udacian - fatima.py
+++ Udacian - fatima.py
@@ -5,19 +5,20 @@ class enrollment:
     
     
 class Udacian:
-    def __init__(self, name,city ,enrollment , nanodgree,status):
+    def __init__(self, name,city ,enrollment , nanodgree,status,count):
         self.name = name
         self.city = city
         self.enrollment = enrollment
         self.nanodgree = nanodgree
         self.status = status
+        self.count = count
          
     def print_udacian(self):
-        print(self.name+' '+self.city+' '+self.enrollment+' '+self.status+' '+self.nanodgree)
+        print(self.name+' '+self.city+' '+self.enrollment+' '+self.status+' '+self.nanodgree+' '+self.count)
      
         
         
              
-p = Udacian('fatima','jeddah','enrolment','full stack ','single')
+p = Udacian('fatima','jeddah','enrolment','full stack ','single ','First time')
 
 p.print_udacian()
